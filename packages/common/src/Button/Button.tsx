02dee9aa0cebd28fedba1fe1a4293d65e6fed655
rojay simpson <harrysmith1991@gmail.com>
2020-08-03 01:31:53 -0500

    init

---
+++ packages/common/src/Button/Button.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Button({ styles }: { styles: any }) {
+  return <button type="button">Click Button</button>;
+}
---
+++ packages/web/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { Button } from "@mono/common";
+
+function App() {
+  return <Button styles={{ x: "color" }}></Button>;
+}
+
+export default App;
